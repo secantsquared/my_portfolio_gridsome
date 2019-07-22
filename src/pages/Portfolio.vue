8d02d750d08721ea68794c1ce2e3f6ad2e1bf5cd
ryanboris <ryan.boris@gmail.com>
2019-07-22 09:48:44 -0400

    :construction: Remove styles from port page

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -12,27 +12,11 @@ export default {
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Geostar&display=swap');
-div.container {
-  max-width: 80vw;
-  margin: 0 auto;
-  height: 100vh;
-  background: #252a34;
-  border-radius: 40px;
-  z-index: -1;
-  padding: 20px;
-}
 
 h2 {
   text-align: center;
   font-size: 7rem;
   letter-spacing: 2px;
-  color: #eaeaea;
   font-family: 'Geostar', cursive;
-}
-
-div.top {
-  width: 100%;
-  height: 350px;
-  background: #252a34;
 }
 </style>
